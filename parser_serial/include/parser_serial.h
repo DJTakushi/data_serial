8fef08a9ea5b9b051dce4cf8cc8ae8d7612b4a9b
danny <dannytakushi@gmail.com>
2024-11-25 06:53:35 -0600

    parser_serial flesh-out

--- parser_serial/include/parser_serial.h
+++ parser_serial/include/parser_serial.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "parser_i.h"
-
+#include "attribute_datatype_enum.h"
+struct serial_def{
+  std::string name_;
+  uint pos_;
+  Datatype type_;
+};
 class parser_serial : public parser_i {
+  std::map<uint,serial_def> def_map_;
   void configure(nlohmann::json config);
-  nlohmann::json get_data(void*);
+  nlohmann::json get_data(void* data);
   nlohmann::json get_all_supported_attributes();
   nlohmann::json get_config();
 };
--- parser_serial/src/parser_serial.cpp
+++ parser_serial/src/parser_serial.cpp
@@ -1,11 +1,49 @@
+#include <boost/algorithm/string.hpp>
+#include <chrono>
+#include "time_helper.h"
 #include "parser_serial.h"
+typedef std::chrono::system_clock::time_point sys_tp;
 
 void parser_serial::configure(nlohmann::json config){
   /** TODO: */
 }
-nlohmann::json parser_serial::get_data(void*){
+nlohmann::json parser_serial::get_data(void* data){ // TODO : rename get_attributes_from_data
   nlohmann::json j;
-  /** TODO: */
+  std::string* str = (std::string*)(data);
+  sys_tp time = std::chrono::system_clock::now();
+  std::vector<std::string> parts;
+  boost::split(parts,*str,boost::is_any_of(","));
+
+  size_t counter = 0;
+  auto epoch = std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()).count(); // TODO : creat common implementation so users don't need chrono
+  for(auto part : parts) {
+    nlohmann::json attr;
+    if(counter < def_map_.size()){
+      boost::trim(part);
+
+      serial_def& def = def_map_[counter];
+
+      std::string metric_name = "metric"+std::to_string(counter);
+      attr["name"] = def.name_;
+      attr["datatype"] = def.type_;
+      switch (def.type_){
+        case kInteger:
+          attr["value"] = std::stoi(part);
+          break;
+        case kDouble:
+          attr["value"] = std::stod(part);
+          break;
+        case kString:
+        default:
+          attr["value"] = part;
+      }
+
+      attr["timestamp"] = epoch;
+
+      j.emplace_back(attr);
+    }
+    counter++;
+  }
   return j;
 }
 nlohmann::json parser_serial::get_all_supported_attributes(){
