b1b13cbd1ac139d9f88223648fee52aab8bcb34b
danny <dannytakushi@gmail.com>
2024-11-29 22:42:34 -0600

    get_connection_type()

--- app/main.cpp
+++ app/main.cpp
@@ -17,13 +17,27 @@ void sig_int_handler(int signum) {
   exit_application(1);
 }
 
+connection_type get_connection_type(int argc, char* argv[]){
+  connection_type type = kMqtt;
+  for(size_t i = 1; i < argc; i++) {
+    std::string str(argv[i]);
+    if (str.substr(0,2)=="-c"){
+      if(str.substr(2,str.npos)==AZURE_ROUTES){
+        type = kAzureIot;
+        std::cout << "connecion type '"<<AZURE_ROUTES<<"' detected"<<std::endl;
+      }
+      if(str.substr(2,str.npos)==MQTT){
+        type = kAzureIot;
+        std::cout << "connecion type '"<<MQTT<<"' detected"<<std::endl;
+      }
+    }
+  }
+  return type;
+};
+
 
 int main(int argc, char* argv[]) {
-  #ifdef AZURE_ROUTES
-    connection_type type = kAzureIot;
-  #else
-    connection_type type = kMqtt;
-  #endif
+  connection_type type = get_connection_type(argc,argv);
 
   data_serial_ = data_serial_factory::create("data_serial",
                                               "data_serial_output",
