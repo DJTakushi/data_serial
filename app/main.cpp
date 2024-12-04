6f86beb01461491a33f61689a8c670f8925c0e8c
danny <dannytakushi@gmail.com>
2024-12-04 15:16:47 -0600

    argument_helper functions replace clutter in main

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,6 @@
 #include <csignal>
 #include <iostream>
+#include "argument_helper.h"
 #include "data_serial_factory.h"
 
 
@@ -18,50 +19,12 @@ void sig_int_handler(int signum) {
   exit_application(1);
 }
 
-connection_type get_connection_type(int argc, char* argv[]){
-  connection_type type = kMqtt;
-  for(size_t i = 1; i < argc; i++) {
-    std::string str(argv[i]);
-    if (str.substr(0,2)=="-c"){
-      if(str.substr(2,str.npos)==AZURE_ROUTES){
-        type = kAzureIot;
-        std::cout << "connecion type '"<<AZURE_ROUTES<<"' detected"<<std::endl;
-      }
-      if(str.substr(2,str.npos)==MQTT){
-        type = kMqtt;
-        std::cout << "connecion type '"<<MQTT<<"' detected"<<std::endl;
-      }
-    }
-  }
-  return type;
-};
-std::string get_address(int argc, char* argv[]){
-  std::string address = "localhost";
-  for(size_t i = 1; i < argc; i++){
-      std::string str(argv[i]);
-    if (str.substr(0,2)=="-a"){
-      address = str.substr(2,str.npos);
-    }
-  }
-  return address;
-}
-uint get_port(int argc, char* argv[]){
-  uint port = 1883;
-  for(size_t i = 1; i < argc; i++){
-      std::string str(argv[i]);
-    if (str.substr(0,2)=="-p"){
-      port = std::stoi(str.substr(2,str.npos));
-    }
-  }
-  return port;
-}
-
 int main(int argc, char* argv[]) {
   signal(SIGINT, sig_int_handler); // registar for ctrl-c
   signal(SIGTERM, exit_application); // terminate from Docker STOPSIGNAL
-  connection_type type = get_connection_type(argc,argv);
-  std::string address = get_address(argc,argv);
-  uint port = get_port(argc,argv);
+  connection_type type = argument_helper::get_connection_type(argc,argv);
+  std::string address = argument_helper::get_address(argc,argv);
+  uint port = argument_helper::get_port(argc,argv);
 
   data_serial_ = data_serial_factory::create("data_serial",
                                               "data_serial_output",
